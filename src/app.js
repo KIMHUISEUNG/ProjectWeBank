ec4658a3cda9d12eaf2994c2d44bf5444761e37e
jeong min <jjgmt0917@gmail.com>
2021-11-17 22:11:21 +0900

    jm : update image source path

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ import express from 'express'
 import mainRouter from './public/routers/mainRouter'
 import userRouter from './public/routers/userRouter'
 import toonRouter from './public/routers/toonRouter'
+import { render } from 'pug'
 /**const { request } = require('http')*/
 
 const PORT = 5050
@@ -14,6 +15,8 @@ const app = express()
 app.use(express.json())
 app.set('views', process.cwd() + '/src/views')
 app.set('view engine', 'pug')
+
+app.use('/public', express.static('src/public'))
 
 app.use('/', mainRouter)
 app.use('/user', userRouter)
