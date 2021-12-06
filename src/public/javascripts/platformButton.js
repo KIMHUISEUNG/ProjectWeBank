e989bbba742fe0cf815e1062dcc01ef17c01493f
jeong min <jjgmt0917@gmail.com>
2021-12-06 22:26:08 +0900

    create platform button

--- src/public/javascripts/platformButton.js
+++
@@ -1,8 +0,0 @@
-const link = document.querySelectorAll('.link')
-
-if (link.match('comic.naver.com')) button((type = 'button')) | 네이버
-
-console.dir(link)
-// if (toon.platform.includes('comic.naver.com'))
-//   button((type = 'button')) | 네이버
-console.log('ddd')
