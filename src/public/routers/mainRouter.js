1fa9025e912f775bc7de1f239975042b612743a3
jeong min <jjgmt0917@gmail.com>
2021-12-06 17:33:48 +0900

    install modules

--- src/public/routers/mainRouter.js
+++ src/public/routers/mainRouter.js
@@ -1,12 +1,12 @@
 // @ts-check
 
 import express from 'express'
-import { join, login } from '../controllers/userController'
+import { joinMembership, login } from '../controllers/userController'
 import { home, search } from '../controllers/toonController'
 const mainRouter = express.Router()
 
 mainRouter.get('/', home)
-mainRouter.get('/join', join)
+mainRouter.get('/join', joinMembership)
 mainRouter.get('/login', login)
 mainRouter.get('/search', search)
 
