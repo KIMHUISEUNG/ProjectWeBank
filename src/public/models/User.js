2d9b49b06d4a12e62a811c45feef868edffb7008
jeong min <jjgmt0917@gmail.com>
2021-12-08 23:27:38 +0900

    connect loginpage and login function

--- src/public/models/User.js
+++ src/public/models/User.js
@@ -2,9 +2,9 @@ import bcrypt from 'bcrypt'
 import mongoose from 'mongoose'
 
 const userSchema = new mongoose.Schema({
-  email: { type: String, required: true, unique: true },
-  username: { type: String, required: true, unique: true },
+  username: { type: String, required: true, unique: true }, //얘가 id
   password: { type: String, required: true, minlength: 8 },
+  email: { type: String, required: true, unique: true },
   name: { type: String, required: true },
   gender: { type: String },
 })
--- src/public/routers/userRouter.js
+++ src/public/routers/userRouter.js
@@ -1,12 +1,10 @@
 import express from 'express'
-import { edit, remove, login, logout } from '../controllers/userController'
+import { edit, remove, logout } from '../controllers/userController'
 
 const userRouter = express.Router()
 
-userRouter.get('/login', login)
 userRouter.get('/logout', logout)
 userRouter.get('/edit', edit)
 userRouter.get('/remove', remove)
-userRouter.get('/:id', login)
 
 export default userRouter
